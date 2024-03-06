97569136b61748a3a16702a81c1feb65c4504a3a
Aman Kumar <kumaraman140795@gmail.com>
2024-03-06 15:45:54 +0530

    Add memoization for increment and decrement functions

--- src/hooks/useCount/useCount.jsx
+++ src/hooks/useCount/useCount.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const useCount = (initialValue) => {
     const [count, setCount] = useState(initialValue);
 
-    const increment = () => {
+    const increment = useCallback(() => {
         setCount(count => count + 1);
-    }
+    }, []);
 
-    const decrement = () => {
+    const decrement = useCallback(() => {
         setCount(count => count - 1);
-    }
+    }, []);
 
     return {
         count,
